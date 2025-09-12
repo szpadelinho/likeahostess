06e1139a4b6f1cdab05727fe3b876b94b742a668
szpadelinho <f.goralski2@outlook.com>
2025-09-12 19:03:46 +0200

    Media corrections

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,5 @@
 import {Yesteryear} from "next/font/google"
+import Image from "next/image";
 
 const yesteryear = Yesteryear({
     weight: "400",
@@ -10,7 +11,7 @@ const Navbar = () => {
         <div
             className={`max-w-screen z-10 h-25 text-center content-center justify-center items-center flex flex-row text-[40px] text-white`}>
             <div className={"flex content-center justify-center items-center"}>
-                <h1 className={yesteryear.className}>Like a Hostess</h1>
+                <Image src={"/images/dragon2.png"} alt={"Dragon navbar icon"} height={50} width={250} />
             </div>
         </div>
     )
