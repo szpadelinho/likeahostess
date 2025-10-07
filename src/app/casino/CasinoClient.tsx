1cc77dda218524a11dbb2556328c3eee1c3d6a1c
szpadelinho <f.goralski2@outlook.com>
2025-10-07 18:23:45 +0200

    chohan gambling game

--- src/app/casino/CasinoClient.tsx
+++ src/app/casino/CasinoClient.tsx
@@ -2,20 +2,64 @@
 
 import ReactPlayer from "react-player";
 import React, {useEffect, useState} from "react";
-import {LogOut, Volume2, VolumeOff} from "lucide-react";
+import {JapaneseYen, LogOut, SquareDashedMousePointer, Volume2, VolumeOff} from "lucide-react";
 import {useRouter} from "next/navigation";
 import {Yesteryear} from "next/font/google";
 import Image from "next/image";
+import CasinoGame from "@/app/casino/CasinoGame";
 
 const yesteryear = Yesteryear({
     weight: "400",
     subsets: ['latin'],
 })
 
+type Club = {
+    name: string
+    host: {
+        name: string
+        surname: string
+        image: string
+    },
+    money: number
+    popularity: number
+    logo: string
+}
+
 const CasinoClient = () => {
-    const router = useRouter();
+    const router = useRouter()
+
+    const [club, setClub] = useState<Club>()
+    useEffect(() => {
+        const stored = localStorage.getItem("selectedClub")
+        if (!stored) return
+
+        const clubData = JSON.parse(stored)
+
+        fetch(`/api/user-club?clubId=${clubData.id}`, {method: "POST"})
+            .then(async (res) => {
+                const data = await res.text()
+                if (!res.ok) {
+                    console.error("API error:", res.status, data)
+                    throw new Error("Failed to fetch club data")
+                }
+                return JSON.parse(data)
+            })
+            .then((userData) => {
+                const mergedClub: Club = {
+                    name: clubData.name,
+                    host: clubData.host,
+                    logo: clubData.logo,
+                    money: userData.money,
+                    popularity: userData.popularity,
+                }
+                setClub(mergedClub)
+            })
+    }, [])
+
     const [isPlaying, setIsPlaying] = useState(true)
     const [muted, setMuted] = useState(true)
+
+    const [game, setGame] = useState<"Roulette" | "Blackjack" | "Chohan" | null>(null)
 
     useEffect(() => {
         const timer = setTimeout(() => {
@@ -30,7 +74,7 @@ const CasinoClient = () => {
             <button onClick={() => {
                 router.push("/")
             }}
-                    className={"z-50 absolute top-10 right-10 border-white border-2 rounded-[10] p-2 cursor-alias hover:bg-white hover:text-black transition-all duration-200 ease-in-out transform active:scale-110 text-white"}>
+                    className={"backdrop-blur-xl z-50 absolute top-10 right-10 border-white border-2 rounded-[10] p-2 cursor-alias hover:bg-white hover:text-black transition-all duration-200 ease-in-out transform active:scale-110 text-white"}>
                 <LogOut/>
             </button>
             <button onClick={() => {
@@ -41,11 +85,19 @@ const CasinoClient = () => {
                     setIsPlaying(true)
                 }
             }}
-                    className={"z-50 absolute top-10 right-25 border-white border-2 rounded-[10] p-2 cursor-alias hover:bg-white hover:text-black transition-all duration-200 ease-in-out transform active:scale-110 text-white"}>
+                    className={"backdrop-blur-xl z-50 absolute top-10 right-25 border-white border-2 rounded-[10] p-2 cursor-alias hover:bg-white hover:text-black transition-all duration-200 ease-in-out transform active:scale-110 text-white"}>
                 {
                     isPlaying ? <Volume2/> : <VolumeOff/>
                 }
             </button>
+            {game && (
+                <button onClick={() => {
+                    setGame(null)
+                }}
+                        className={"backdrop-blur-xl z-50 absolute top-10 left-10  border-white border-2 rounded-[10] p-2 cursor-alias hover:bg-white hover:text-black transition-all duration-200 ease-in-out transform active:scale-110 text-white"}>
+                    <SquareDashedMousePointer/>
+                </button>
+            )}
             <ReactPlayer
                 src={"https://youtube.com/embed/8mqRTo74g-0?autoplay=1"}
                 playing={isPlaying}
@@ -56,22 +108,80 @@ const CasinoClient = () => {
                 className={"flex absolute top-0 left-0 z-[-1]"}
                 style={{height: '0px', width: '0px', visibility: 'hidden', position: 'absolute'}}
             />
-            <Image className={"absolute -left-10 top-60"} src={"/images/tanimura_cover.png"} alt={"Masayoshi Tanimura"} height={0} width={400}/>
-            <h1 className={`absolute top-20 text-[50px] ${yesteryear.className}`}>What will we play today?</h1>
-            <div className={"flex flex-row items-center justify-center gap-10"}>
-                <div className={"backdrop-blur-xl gap-10 border-2 border-white flex flex-col items-center justify-center rounded-[15] max-w-[350px] p-5 h-125 hover:bg-white hover:text-black duration-200 ease-in-out hover:shadow-md hover:shadow-white"}>
-                    <h1 className={`text-[40px] ${yesteryear.className}`}>Roulette</h1>
-                    <p className={"text-[20px]"}>A classic game. The dealer places a metal ball inside of the roulette, which is rolling through the number spots. The players have multiple possibilities to bet over - number, color, row, odd/even, interval and many others.</p>
+            {!game ? (
+                <>
+                    <Image className={"absolute -left-10 top-60"} src={"/images/tanimura_cover.png"} alt={"Masayoshi Tanimura"} height={0} width={400}/>
+                    <h1 className={`absolute top-20 text-[50px] ${yesteryear.className}`}>What will we play today?</h1>
+                    <div className={"flex flex-row items-center justify-center gap-10"}>
+                        <div className={"backdrop-blur-xl gap-10 border-2 border-white flex flex-col items-center justify-center rounded-[15] max-w-[350px] p-5 h-125 hover:bg-white hover:text-black duration-200 ease-in-out hover:shadow-md hover:shadow-white"}
+                             onClick={() => {
+                                 if(!game) {
+                                     setGame("Roulette")
+                                 }
+                                 else {
+                                     setGame(null)
+                                 }
+                             }}>
+                            <h1 className={`text-[40px] ${yesteryear.className}`}>Roulette</h1>
+                            <p className={"text-[20px]"}>A classic game. The dealer places a metal ball inside of the roulette, which is rolling through the number spots. The players have multiple possibilities to bet over - number, color, row, odd/even, interval and many others.</p>
+                        </div>
+                        <div className={"backdrop-blur-xl gap-10 border-2 border-white flex flex-col items-center justify-center rounded-[15] max-w-[350px] p-5 h-125 hover:bg-white hover:text-black duration-200 ease-in-out hover:shadow-md hover:shadow-white"}
+                             onClick={() => {
+                                 if(!game) {
+                                     setGame("Blackjack")
+                                 }
+                                 else {
+                                     setGame(null)
+                                 }
+                             }}>
+                            <h1 className={`text-[40px] ${yesteryear.className}`}>Blackjack</h1>
+                            <p className={"text-[20px]"}>A card game. The dealer gives himself two cards - one visible and one hidden. The players however receive two visible cards. The goal for the players is to have a bigger score than the dealer. However, when the score is bigger than 21 - the game is over. If you score a 21, a "Blackjack" is being called.</p>
+                        </div>
+                        <div className={"backdrop-blur-xl gap-10 border-2 border-white flex flex-col items-center justify-center rounded-[15] max-w-[350px] p-5 h-125 hover:bg-white hover:text-black duration-200 ease-in-out hover:shadow-md hover:shadow-white"}
+                             onClick={() => {
+                                 if(!game) {
+                                     setGame("Chohan")
+                                 }
+                                 else {
+                                     setGame(null)
+                                 }
+                             }}>
+                            <h1 className={`text-[40px] ${yesteryear.className}`}>Chō-Han</h1>
+                            <p className={"text-[20px]"}>Even versus Odd. The dealer is shaking a bamboo bowl, in which two dices are being shaken. In this game, the goal is to predict what the sum of two dices will be. You either call it the sum to be even (chō) or odd (han).</p>
+                        </div>
+                    </div>
+                </>
+            ):(
+                <CasinoGame game={game}/>
+            )}
+            {club && (
+                <div
+                    className={`backdrop-blur-md absolute left-5 bottom-5 w-130 h-40 text-center content-center items-center flex flex-row text-[20px] rounded-[20] text-white z-50`}
+                    style={{backgroundColor: "rgba(0, 0, 0, .1)"}}>
+                    <div className={"backdrop-blur-xl h-[130%] w-[40%] rounded-[20] flex justify-center relative"}
+                         style={{backgroundColor: "rgba(0, 0, 0, .1)"}}>
+                        <Image
+                            className={"flex absolute bottom-[-80%]"}
+                            src={club.host.image}
+                            alt={"Host"}
+                            height={500}
+                            width={150}
+                        />
+                    </div>
+                    <div className={"flex flex-row text-center justify-center content-center w-[60%] h-[100%] p-5"}>
+                        <div className={"flex flex-col justify-center w-[60%]"}>
+                            <h1 className={"text-[22px] font-[700]"}>{club.host.name} {club.host.surname}</h1>
+                            <h2 className={"text-[18px] font-[600]"}>{club.name}</h2>
+                        </div>
+                        <div className={"flex flex-col justify-center w-[40%]"}>
+                            <h2 className={"text-[20px] font-[400] flex flex-row justify-center items-center"}>
+                                <JapaneseYen/>
+                                <p>{club.money}</p>
+                            </h2>
+                        </div>
+                    </div>
                 </div>
-                <div className={"backdrop-blur-xl gap-10 border-2 border-white flex flex-col items-center justify-center rounded-[15] max-w-[350px] p-5 h-125 hover:bg-white hover:text-black duration-200 ease-in-out hover:shadow-md hover:shadow-white"}>
-                    <h1 className={`text-[40px] ${yesteryear.className}`}>Blackjack</h1>
-                    <p className={"text-[20px]"}>A card game. The dealer gives himself two cards - one visible and one hidden. The players however receive two visible cards. The goal for the players is to have a bigger score than the dealer. However, when the score is bigger than 21 - the game is over. If you score a 21, a "Blackjack" is being called.</p>
-                </div>
-                <div className={"backdrop-blur-xl gap-10 border-2 border-white flex flex-col items-center justify-center rounded-[15] max-w-[350px] p-5 h-125 hover:bg-white hover:text-black duration-200 ease-in-out hover:shadow-md hover:shadow-white"}>
-                    <h1 className={`text-[40px] ${yesteryear.className}`}>Chō-Han</h1>
-                    <p className={"text-[20px]"}>Even versus Odd. The dealer is shaking a bamboo bowl, in which two dices are being shaken. In this game, the goal is to predict what the sum of two dices will be. You either call it the sum to be even (chō) or odd (han).</p>
-                </div>
-            </div>
+            )}
         </div>
     )
 }
---
+++ src/app/casino/CasinoGame.tsx
@@ -0,0 +1,130 @@
+import React, {useState} from "react";
+import {Yesteryear} from "next/font/google";
+import {CircleSmall, GlassWater} from "lucide-react";
+
+const yesteryear = Yesteryear({
+    weight: "400",
+    subsets: ['latin'],
+})
+
+interface CasinoGameProps {
+    game: "Roulette" | "Blackjack" | "Chohan" | null
+}
+
+const CasinoGame = ({game}: CasinoGameProps) => {
+    const [score, setScore] = useState<boolean | null>(null)
+    const [value, setValue] = useState<string>("")
+    const [total, setTotal] = useState<number>(0)
+    const [array, setArray] = useState<number[]>([])
+
+    const handleGame = (type: string, value: string) => {
+        if (type === "Chohan") {
+            let sum = Array(2)
+            for(let i = 0; i < 2; i++) {
+                sum[i] = Math.floor(Math.random() * (6 - 1)) + 1
+            }
+            const total = sum.reduce((a, b) => a + b, 0)
+            if(total % 2 === 0) {
+                if(value === "Even"){
+                    handleScore("Chohan", "even", total, sum, true)
+                }
+                else{
+                    handleScore("Chohan", "even", total, sum, false)
+                }
+            }
+            else{
+                if(value === "Odd"){
+                    handleScore("Chohan", "odd", total, sum, true)
+                }
+                else{
+                    handleScore("Chohan", "odd", total, sum, false)
+                }
+            }
+        }
+    }
+
+    const handleScore = (type: string, value: string, total: number, array: Array<number>, won: boolean) => {
+        if(type === "Chohan") {
+            setValue(value)
+            setTotal(total)
+            setArray(array)
+            setScore(won)
+        }
+    }
+
+    const renderDice = (value: number) => {
+        return(
+            <div className={"grid grid-cols-3 grid-rows-3"}>
+                {Array.from({ length: 9 }).map((_, i) => {
+                    const positions: Record<number, number[]> = {
+                        1: [4],
+                        2: [0, 8],
+                        3: [0, 4, 8],
+                        4: [0, 2, 6, 8],
+                        5: [0, 2, 4, 6, 8],
+                        6: [0, 2, 3, 5, 6, 8],
+                    }
+                    return positions[value].includes(i) ? (
+                        <CircleSmall key={i} size={50} fill="black" />
+                    ) : (
+                        <div key={i}></div>
+                    )
+                })}
+            </div>
+        )
+    }
+
+    return (
+        <div className={"flex flex-col justify-center items-center"}>
+            {game === "Chohan" && (
+                <>
+                    <h1 className={`text-[75px] ${yesteryear.className}`}>Chō-Han</h1>
+                    <div className={"flex flex-col justify-center items-center gap-5"}>
+                        <div className={"flex flex-row justify-center items-center h-140 gap-5"}>
+                            <GlassWater fill={"white"} size={400} className={"-rotate-180"}/>
+                            <div className={"flex flex-row justify-center items-center gap-5"}>
+                                {array.map((item, i) => (
+                                    <div
+                                        key={i}
+                                        className={"flex justify-center items-center border-2 bg-white rounded-[20px] h-40 w-40"}
+                                    >
+                                        {renderDice(item)}
+                                    </div>
+                                ))}
+                            </div>
+
+                        </div>
+                        <h1 className={`${yesteryear.className} text-[30px]`}>Place your bet</h1>
+                        <div className={"flex flex-row justify-center items-center gap-5"}>
+                            <button
+                                className={`${yesteryear.className} gap-5 flex flex-row backdrop-blur-xl text-[30px] border-white border-2 rounded-[10] p-2 w-35 items-center justify-center cursor-alias hover:bg-white hover:text-black transition-all duration-200 ease-in-out transform active:scale-110 text-white`}
+                                onClick={() => {
+                                    handleGame("Chohan", "Even")
+                                }}>
+                                <p>Even</p>
+                                <CircleSmall fill={"white"}/>
+                            </button>
+                            <button
+                                className={`${yesteryear.className} gap-5 flex flex-row backdrop-blur-xl text-[30px] border-white border-2 rounded-[10] p-2 w-35 items-center justify-center cursor-alias hover:bg-white hover:text-black transition-all duration-200 ease-in-out transform active:scale-110 text-white`}
+                                onClick={() => {
+                                    handleGame("Chohan", "Odd")
+                                }}>
+                                <p>Odd</p>
+                                <CircleSmall/>
+                            </button>
+                        </div>
+                    </div>
+                    {score !== null && (
+                        <h1 className={`${yesteryear.className} absolute bottom-5 right-5 backdrop-blur-md p-2 h-55 w-120 rounded-[20] text-[40px] flex justify-center items-center flex-col`}>
+                            <p>{score ? "You won!" : "You lost."}</p>
+                            <p>{total ? `The sum  ${total} was ${value}.` : "The draw is rigged."}</p>
+                            <p>{array ? `The winning pair was ${array[0]} and ${array[1]}.` : "The draw is rigged."}</p>
+                        </h1>
+                    )}
+                </>
+            )}
+        </div>
+    )
+}
+
+export default CasinoGame
